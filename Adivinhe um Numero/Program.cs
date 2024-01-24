449ad9630f2528caee7be3dadd44d6e6f0d43c4a
BRuno3h3 <bruno.tomasi16@gmail.com>
2024-01-23 22:41:23 -0300

    Adicionar arquivos de projeto.

---
+++ Adivinhe um Numero/Program.cs
@@ -0,0 +1,25 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 10);
+
+do
+{
+    Console.Write("Digite um número entre 1 e 10: ");
+    int chute = int.Parse(Console.ReadLine()!);
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número é maior.");
+    }
+    else
+    {
+        Console.WriteLine("O número é menor.");
+    }
+
+} while (true);
+
+Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
