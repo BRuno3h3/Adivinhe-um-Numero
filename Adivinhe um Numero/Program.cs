51c6315909bd2035729357767b36451385960051
BRuno3h3 <bruno.tomasi16@gmail.com>
2024-07-06 01:19:00 -0300

    Upgrade no jogo

--- Adivinhe um Numero/Program.cs
+++ Adivinhe um Numero/Program.cs
@@ -1,14 +1,23 @@
 ﻿Random aleatorio = new Random();
-int numeroSecreto = aleatorio.Next(1, 10);
+int numeroSecreto = aleatorio.Next(1, 11); // Next(1, 10) gera um número entre 1 e 9, então é melhor usar Next(1, 11)
 
-do
+Console.WriteLine("Digite um número entre 1 e 10: ");
+Console.WriteLine("Você tem 3 chances!!!");
+
+bool acertou = false;
+
+for (int i = 3; i > 0; i--)
 {
-    Console.Write("Digite um número entre 1 e 10: ");
+    if(i != 3)
+    {
+    Console.WriteLine($"Você tem {i} tentativas");
+    }
     int chute = int.Parse(Console.ReadLine()!);
 
     if (chute == numeroSecreto)
     {
         Console.WriteLine("Parabéns! Você acertou o número.");
+        acertou = true;
         break;
     }
     else if (chute < numeroSecreto)
@@ -19,7 +28,13 @@ do
     {
         Console.WriteLine("O número é menor.");
     }
+}
 
-} while (true);
-
-Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
+if (!acertou)
+{
+    Console.WriteLine($"O jogo acabou. Você não acertou o número secreto que era {numeroSecreto}.");
+}
+else
+{
+    Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
+}
